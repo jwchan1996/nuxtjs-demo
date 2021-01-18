4e6191b64ae6dc1d4f16f717e7ab7ac386c23812
jwchan <741755613@qq.com>
2021-01-18 19:29:21 +0800

    feat: 初始化 Nuxt.js 项目

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-page">
+    <h1>Hello Nuxt.js!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style>
+
+</style>
