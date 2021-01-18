2cbcbf03b80c7611220ef6535c640d3f1c27977b
jwchan <741755613@qq.com>
2021-01-18 20:19:00 +0800

    04-动态路由

---
+++ pages/user/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user">
+    <h1>User Page</h1>
+    <p>{{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage'
+}
+</script>
+
+<style>
+
+</style>
