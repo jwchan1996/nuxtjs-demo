476ae1b093da72c2c63b7538135078817b1ba898
jwchan <741755613@qq.com>
2021-01-18 19:58:49 +0800

    feat: 02-router

---
+++ pages/user/abc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user-bac-page">
+    <h1>User Abc Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserAbcPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user">
+    <h1>User Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage'
+}
+</script>
+
+<style>
+
+</style>
