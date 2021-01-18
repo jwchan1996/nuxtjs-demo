5976dd76ca46c5f8bf883f1399a91d7bbdf795e6
jwchan <741755613@qq.com>
2021-01-18 20:26:52 +0800

    05-嵌套路由

---
+++ pages/users.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="users">
+    <h1>Users 父路由</h1>
+    <!-- 子路由出口 -->
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/users/foo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Users Foo 子路由</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersFoo'
+}
+</script>
+
+<style>
+
+</style>
